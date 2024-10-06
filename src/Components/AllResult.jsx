0b1dfb5415e261d9c77cab650af853d757069e73
Javeria Ahmed <jahmedkhan911@gmail.com>
2024-10-06 19:42:39 +0500

    height reset

--- src/Components/AllResult.jsx
+++ src/Components/AllResult.jsx
@@ -24,7 +24,7 @@ function AllResult() {
     );
   } else if (status == STATUS.IDLE) {
     return (
-      <div className="container d-flex flex-wrap gap-3 ">
+      <div className="container d-flex flex-wrap">
         {arrrayofdata.length > 0 ? (
           arrrayofdata.map((val, ind) => <FinalResult key={ind} val={val} />)
         ) : show == true ? (
